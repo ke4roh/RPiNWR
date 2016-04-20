44af3e4c5038a0e3708e1e3c285519016f83e17f
Jim Scarborough <jimes@hiwaay.net>
2016-04-20 11:21:00 -0400

    docs

--- README.md
+++ README.md
@@ -4,5 +4,21 @@ This library adapts the [Raspberry Pi SAME decoder board](http://www.aiwindustri
 functionality so that you can focus on your application.  It has
 error handling!  It has unit tests!  It has events!  Callbacks!
 
+## Install
+Get GPIO working:
+
+```bash
+sudo apt-get update
+sudo apt-get install git python-dev python-smbus i2c-tools
+# Follow instructions to install i2c kernel support:
+#   https://learn.adafruit.com/adafruits-raspberry-pi-lesson-4-gpio-setup/configuring-i2c
+git clone https://github.com/nioinnovation/Adafruit_Python_GPIO.git
+(cd Adafruit_Python_GPIO; sudo python3 setup.py install)
+# now CD to where you cloned this project
+sudo ./setup.py test
+python3 -m RPiNWR.demo
+```
+
 ## License
 GNU GPL v. 3 - see the LICENSE file for details
+
--- setup.py
+++ setup.py
@@ -14,12 +14,16 @@ setup(
     name='RPiNWR',
     version='0.0.1',
     description='Operate the Raspberry Pi NOAA Weather Radio Receiver and Decoder',
-    long_description='This library provides basic operations and demonstration of features for the board',
+    long_description="""
+    This library provides basic operations and demonstration of features for a Si4707-based weather radio
+    running on a Raspberry Pi.
+    """,
     author='Jim Scarborough',
     author_email='jimes@hiwaay.net',
     url='https://github.com/ke4roh/RPiNWR',
     license='GNU GPL v.3',
     packages=find_packages(exclude=('tests', 'docs')),
+    # install_requires=['zlib', 'base64', 'signal', 'logging'],
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     test_suite="tests",
