eb258aeab19188d0e6540ecab0bc981bea9a7435
Yuichiro Nishi <yuh@wni.com>
2016-09-24 13:39:07 -0400

    post process for setup

--- bootstrap/configset/config/set_config.sh
+++ bootstrap/configset/config/set_config.sh
@@ -8,4 +8,17 @@ fi
 
 (cd $MYPATH/$CONFIG ; tar cfp - .) | (cd / ; tar xf -)
 
+if [ "$CONFIG" = "self_ap" ]; then
+	service hostapd stop ; service hostapd start
+	service dhcpd restart
+	ifdown wlan0
+	ifup wlan0
+	ifconfig wlan0 inet 10.0.0.1 netmask 255.255.255.0 
+elif
+	service hostapd stop
+	service dhcpd restart
+	ifdown wlan0
+	ifup wlan0
+fi
+
 
